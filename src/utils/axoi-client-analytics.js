3a55285488686eed87f971c19ddd70dca4890274
Samuel Asimetamunopiri <samuelasime@gmail.com>
2025-06-23 18:18:27 +0100

    axios

--- src/utils/axoi-client-analytics.js
+++ src/utils/axoi-client-analytics.js
@@ -1,28 +1,30 @@
 import axios from "axios";
 
-const axiosClientAnalytics = axios.create ({baseURL: `${
-    process.env.REACT_APP_ANALYTIC_API_BASE_URL }`});
-
-// Create Interceptors
-axiosClientAnalytics.interceptors.request.use ( (config) => {
-    const userToken = localStorage.getItem ("USER_ACCESS_TOKEN");
-    // Check which token is available and set the appropriate Authorization header
-    if (userToken) {
-        const cleanedToken = userToken.replace(/['"]+/g, '');
-        config.headers.Authorization = `Bearer ${cleanedToken}`;
-    }
-    return config;
+const axiosClientAnalytics = axios.create({
+  baseURL: import.meta.env.VITE_ANALYTIC_API_BASE_URL,
 });
 
-axiosClientAnalytics.interceptors.response.use ( (response) => {
-    return response;
-}, (error) => {
-    const {response} = error;
+axiosClientAnalytics.interceptors.request.use((config) => {
+  const userToken = localStorage.getItem("USER_ACCESS_TOKEN");
+  if (userToken) {
+    const cleanedToken = userToken.replace(/['"]+/g, '');
+    config.headers.Authorization = `Bearer ${cleanedToken}`;
+  }
+  return config;
+});
+
+// Add response interceptor
+axiosClientAnalytics.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const { response } = error;
+    // Optional: handle auth errors
     // if (response && response.status >= 400 && response.status < 500) {
-    //     localStorage.removeItem ("USER");
-    //     localStorage.removeItem ("USER_ACCESS_TOKEN");
-    // } 
-    return Promise.reject (error);
-});
+    //   localStorage.removeItem("USER");
+    //   localStorage.removeItem("USER_ACCESS_TOKEN");
+    // }
+    return Promise.reject(error);
+  }
+);
 
 export default axiosClientAnalytics;
